4a60982a00c7b1326c7e962bfe35a70eb5e01229
Gabriel Sibley <gabrielsibley@gmail.com>
2014-09-07 01:32:21 -0300

    Set all textures to import as sprites. Added shotgun GUI, made shotgun manipulate properly.

--- HuntersDuel/Assets/Scripts/Shotgun.cs
+++ HuntersDuel/Assets/Scripts/Shotgun.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class Shotgun : Gun {
 	
-	public enum BoltState {
-		Closed,
-		Up,
-		Back,
+	public enum SlideState {
+		Forward,
+		Back
 	}
 	
 	public GameObject[] m_boltGUI;
-	public GameObject[] m_bulletGUI;
+	public GameObject[] m_magazineGUI;
+	public GameObject m_chamberGUI;
+	public GameObject m_chamberBackGUI;
+	public GameObject m_partialGUI;
 	public GameObject m_bodyGUI;
 	
 	public Color m_bodyGUIColor, m_boltGUIColor, m_bulletGUIColor, m_bulletDeadGUIColor;
@@ -23,13 +25,12 @@ public class Shotgun : Gun {
 	
 	public AudioClip m_gunsfx;
 	
-	public int m_ammoReserve = 4;
-	public int m_reserveMax = 8;
+	public int m_ammoReserve = 5;
+	public int m_reserveMax = 10;
 	
-	protected BoltState m_boltState = BoltState.Closed;
-	protected bool m_chambered = true;
-	protected bool m_deadShell;
-	protected BulletState[] m_magazine = new BulletState[5];
+	protected SlideState m_slideState = SlideState.Forward;
+	protected BulletState m_chamber;
+	protected BulletState[] m_magazine = new BulletState[4];
 	protected bool m_partialReload;
 	
 	public override int Damage { get { return 2; } }	
@@ -37,43 +38,36 @@ public class Shotgun : Gun {
 	bool MagazineFull{get{return m_magazine[m_magazine.Length-1] != BulletState.Empty;}}
 	
 	public override void ManipulateUp(){
-		if(m_boltState == BoltState.Closed){
-			m_boltState = BoltState.Up;
-		}
-		else if(m_boltState == BoltState.Back && !MagazineFull && m_ammoReserve > 0){
+		if(m_slideState == SlideState.Forward && !MagazineFull && m_ammoReserve > 0){
 			m_partialReload = true;
 		}
 	}
 	public override void ManipulateDown(){
-		if(m_boltState == BoltState.Up){
-			m_boltState = BoltState.Closed;
-		}
-		else if(m_boltState == BoltState.Back && m_partialReload)
-		{
-			m_ammoReserve--;
-			m_partialReload = false;
-			AddLiveRound();
-		}
+		//Doesnt do anything
 	}
 	public override void ManipulateLeft(){
-		if(m_boltState == BoltState.Up){
-			m_boltState = BoltState.Back;
-			if(m_magazine[0] == BulletState.Fired){
-				RemoveTopRound();
-			}
+		if(m_slideState == SlideState.Forward){
+			m_partialReload = false;
+			m_slideState = SlideState.Back;
+			m_chamber = m_magazine[0];
+			RemoveTopRound();
 		}
 	}
 	public override void ManipulateRight(){
-		if(m_boltState == BoltState.Back){
-			m_boltState = BoltState.Up;
+		if(m_partialReload){
 			m_partialReload = false;
+			m_ammoReserve--;
+			AddLiveRound();
+		}
+		if(m_slideState == SlideState.Back){
+			m_slideState = SlideState.Forward;
 		}
 	}
 	
 	public override bool Fire(BasicMove player, float angle){
-		if(m_magazine[0] == BulletState.Live && m_boltState == BoltState.Closed){
+		if(m_chamber == BulletState.Live && m_slideState == SlideState.Forward){
 			AudioManager.PlaySFX(m_gunsfx);
-			m_magazine[0] = BulletState.Fired;
+			m_chamber = BulletState.Fired;
 			
 			for(int i = 0; i < m_pelletCount; i++){
 				GameObject bullet = Instantiate(m_bulletPrefab, player.transform.position, Quaternion.identity) as GameObject;
@@ -90,12 +84,15 @@ public class Shotgun : Gun {
 		for(int i = 0; i < m_boltGUI.Length; i++){
 			m_boltGUI[i].renderer.material.color = m_boltGUIColor;
 		}
-		for(int i = 0; i < m_bulletGUI.Length; i++){
-			m_bulletGUI[i].renderer.material.color = m_bulletGUIColor;
+		for(int i = 0; i < m_magazineGUI.Length; i++){
+			m_magazineGUI[i].renderer.material.color = m_bulletGUIColor;
 		}
-		for(int i = 0; i < 5; i++){
-			AddLiveRound();
+
+		for(int i = 0; i < m_magazine.Length; i++){
+			m_magazine[i] = BulletState.Live;
 		}
+		m_chamber = BulletState.Live;
+		m_partialGUI.renderer.material.color = m_bulletGUIColor;
 	}
 	
 	void AddLiveRound(){
@@ -114,22 +111,26 @@ public class Shotgun : Gun {
 	
 	void Update(){
 		//Update UI
-		m_boltGUI[0].SetActive(m_boltState == BoltState.Closed);
-		m_boltGUI[1].SetActive(m_boltState == BoltState.Up);
-		m_boltGUI[2].SetActive(m_boltState == BoltState.Back);
+		m_boltGUI[0].SetActive(m_slideState == SlideState.Forward);
+		m_boltGUI[1].SetActive(m_slideState == SlideState.Back);
 		
-		for(int i = 0; i < 5; i++){
-			m_bulletGUI[i].SetActive(m_magazine[i] != BulletState.Empty);
+		for(int i = 0; i < m_magazineGUI.Length; i++){
+			m_magazineGUI[i].SetActive(m_magazine[i] != BulletState.Empty);
 		}
-		if(m_boltState != BoltState.Back && m_magazine[0] != BulletState.Empty){
-			m_bulletGUI[0].SetActive(false);
-			m_bulletGUI[5].SetActive(true);
-			m_bulletGUI[5].renderer.material.color = m_magazine[0] == BulletState.Fired ? m_bulletDeadGUIColor : m_bulletGUIColor;
+		if(m_chamber == BulletState.Empty || m_slideState == SlideState.Back){
+			m_chamberGUI.SetActive (false);
 		}
 		else{
-			m_bulletGUI[5].SetActive(false);
-			m_bulletGUI[0].renderer.material.color = m_magazine[0] == BulletState.Fired ? m_bulletDeadGUIColor : m_bulletGUIColor;
+			m_chamberGUI.SetActive (true);
+			m_chamberGUI.renderer.material.color = m_chamber == BulletState.Fired ? m_bulletDeadGUIColor : m_bulletGUIColor;
 		}
-		m_bulletGUI[6].SetActive(m_partialReload);
+		if(m_chamber == BulletState.Empty || m_slideState == SlideState.Forward){
+			m_chamberBackGUI.SetActive(false);
+		}
+		else{
+			m_chamberBackGUI.SetActive (true);
+			m_chamberBackGUI.renderer.material.color = m_chamber == BulletState.Fired ? m_bulletDeadGUIColor : m_bulletGUIColor;
+		}
+		m_partialGUI.SetActive(m_partialReload);
 	}
 }
